c26a057415f7b9a5f6534dcd51e16ed5b739bb2a
W1nU <jsw9808@gmail.com>
2021-10-16 22:36:24 +0900

    기능 : 값 캡쳐 구현

--- manager.py
+++ manager.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import argparse
+from gaze_tracking.gaze_tracking import GazeTracking
+from Headpose_Detection import headpose
 
 from enum import Enum
 
@@ -11,16 +14,27 @@ class CalibrationStep(Enum):
 
 class Manager:
     def __init__(self):
-        self.calibration = False
+        self.gaze = GazeTracking()
+        self.headpose = None
+        self.camera = None
+
+    def start_camera_capture(self, args):
+        cap = cv2.VideoCapture(0)
+        cap.set(3, args['wh'][0])
+        cap.set(4, args['wh'][1])
+        self.camera = cap
+
+    def initialize_headpose(self, args):
+        self.headpose = headpose.HeadposeDetection(args["landmark_type"], args["landmark_predictor"])
 
     def make_calibration_screen(self, step):
         background = np.zeros((480, 720, 3), np.uint8)
         background.fill(255)
         background = cv2.putText(background, "Look at the point and press space", (76, 240), cv2.FONT_HERSHEY_DUPLEX, 1, (147, 58, 31), 2)
 
-
         if step == CalibrationStep.UP:
             background = cv2.circle(background, (360,12), 1, 255, 24)
+
         elif step == CalibrationStep.RIGHT:
             background = cv2.circle(background, (708,240), 1, 255, 24)
 
@@ -31,13 +45,60 @@ class Manager:
             background = cv2.circle(background, (12,240), 1, 255, 24)
             
         return background
+    
+    def make_configuration(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', metavar='FILE', dest='input_file', default=None, help='Input video. If not given, web camera will be used.')
+        parser.add_argument('-o', metavar='FILE', dest='output_file', default=None, help='Output video.')
+        parser.add_argument('-wh', metavar='N', dest='wh', default=[720, 480], nargs=2, help='Frame size.')
+        parser.add_argument('-lt', metavar='N', dest='landmark_type', type=int, default=1, help='Landmark type.')
+        parser.add_argument('-lp', metavar='FILE', dest='landmark_predictor', 
+                            default='./model/shape_predictor_68_face_landmarks.dat', help="Landmark predictor data file.")
+        args = vars(parser.parse_args())
 
-    def run(self):
+        return args
+
+    def process_calibration_value(self):
+        ret, camera = self.camera.read()
+        
+        camera = cv2.flip(camera, 1)
+        camera, angles = self.headpose.process_image(camera)
+
+        self.gaze.refresh(camera)
+        horizontal_pulpil_ratio = self.gaze.horizontal_ratio()
+        vertical_pulpil_ratio = self.gaze.vertical_ratio()
+
+        print(f"""
+캘리브레이션 값 캡쳐
+Headpose rx : {angles[0]}
+Headpose ry : {angles[1]}
+Headpose rz : {angles[2]}
+Gaze Horizontal : {horizontal_pulpil_ratio}
+Gaze Vertical : {vertical_pulpil_ratio}
+        """)
+
+
+    def calibration(self):
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.UP))
         cv2.waitKey()
+        self.process_calibration_value()
+        
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.RIGHT))
         cv2.waitKey()
+        self.process_calibration_value()
+
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.DOWN))
         cv2.waitKey()
+        self.process_calibration_value()
+
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.LEFT))
         cv2.waitKey()
+        self.process_calibration_value()
+
+    def run(self):
+        configuration = self.make_configuration()
+
+        self.start_camera_capture(configuration)
+        self.initialize_headpose(configuration)
+        self.calibration()
+
