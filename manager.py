b49ded459b9091252c174f91117427bb0e0815e1
W1nU <jsw9808@gmail.com>
2021-10-16 23:20:38 +0900

    델타 계산

--- manager.py
+++ manager.py
@@ -12,8 +12,36 @@ class CalibrationStep(Enum):
     DOWN = 2
     LEFT = 3
 
+class CalibrationValue:
+    def __init__(self):
+        self.head_move = {
+            "left": None,
+            "right": None,
+            "up": None,
+            "down": None
+        }
+
+        self.pulpil_move = {
+            "left": None,
+            "right": None,
+            "up": None,
+            "down": None
+        }
+
+        self.delta = {
+            "horizontal": None,
+            "vertical": None
+        }
+
+    def calc_delta(self):
+        self.delta['horizontal'] = (abs(self.head_move['right']['ry'] - self.head_move['left']['ry'])) / (abs(self.pulpil_move['right']['horizontal'] - self.pulpil_move['left']['horizontal']))
+        self.delta['vertical'] = abs(self.head_move['up']['rx'] - self.head_move['down']['rx']) / abs(self.pulpil_move['up']['vertical'] - self.pulpil_move['down']['vertical'])
+        
+        print(self.delta)
+
 class Manager:
     def __init__(self):
+        self.calibration_value = CalibrationValue()
         self.gaze = GazeTracking()
         self.headpose = None
         self.camera = None
@@ -27,10 +55,11 @@ class Manager:
     def initialize_headpose(self, args):
         self.headpose = headpose.HeadposeDetection(args["landmark_type"], args["landmark_predictor"])
 
-    def make_calibration_screen(self, step):
+    def make_calibration_screen(self, step, movement="Head"):
         background = np.zeros((480, 720, 3), np.uint8)
         background.fill(255)
-        background = cv2.putText(background, "Look at the point and press space", (76, 240), cv2.FONT_HERSHEY_DUPLEX, 1, (147, 58, 31), 2)
+        background = cv2.putText(background, f"Look at the point and press space", (76, 240), cv2.FONT_HERSHEY_DUPLEX, 1, (147, 58, 31), 2)
+        background = cv2.putText(background, f"({movement})", (300 , 280), cv2.FONT_HERSHEY_DUPLEX, 1, (147, 58, 31), 2)
 
         if step == CalibrationStep.UP:
             background = cv2.circle(background, (360,12), 1, 255, 24)
@@ -58,7 +87,9 @@ class Manager:
 
         return args
 
-    def process_calibration_value(self):
+    def process_calibration_value(self, step, movement):
+        target_value = self.calibration_value.head_move if movement == "Head"else self.calibration_value.pulpil_move
+
         ret, camera = self.camera.read()
         
         camera = cv2.flip(camera, 1)
@@ -67,7 +98,14 @@ class Manager:
         self.gaze.refresh(camera)
         horizontal_pulpil_ratio = self.gaze.horizontal_ratio()
         vertical_pulpil_ratio = self.gaze.vertical_ratio()
-
+        value = {
+            "rx": angles[0],
+            "ry": angles[1],
+            "rz": angles[2],
+            "vertical": vertical_pulpil_ratio,
+            "horizontal": horizontal_pulpil_ratio
+        }
+        
         print(f"""
 캘리브레이션 값 캡쳐
 Headpose rx : {angles[0]}
@@ -77,24 +115,54 @@ Gaze Horizontal : {horizontal_pulpil_ratio}
 Gaze Vertical : {vertical_pulpil_ratio}
         """)
 
+        if step == CalibrationStep.UP:
+            target_value['up'] = value
+        elif step == CalibrationStep.LEFT:
+            target_value['left'] = value
+        elif step == CalibrationStep.DOWN:
+            target_value['down'] = value
+        else:
+            target_value['right'] = value
 
     def calibration(self):
+        movement = "Head"
+        
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.UP))
         cv2.waitKey()
-        self.process_calibration_value()
+        self.process_calibration_value(CalibrationStep.UP, movement)
         
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.RIGHT))
         cv2.waitKey()
-        self.process_calibration_value()
+        self.process_calibration_value(CalibrationStep.RIGHT, movement)
 
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.DOWN))
         cv2.waitKey()
-        self.process_calibration_value()
+        self.process_calibration_value(CalibrationStep.DOWN, movement)
 
         cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.LEFT))
         cv2.waitKey()
-        self.process_calibration_value()
+        self.process_calibration_value(CalibrationStep.LEFT, movement)
 
+        movement = "Pupil"
+
+        cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.UP, movement))
+        cv2.waitKey()
+        self.process_calibration_value(CalibrationStep.UP, movement)
+        
+        cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.RIGHT, movement))
+        cv2.waitKey()
+        self.process_calibration_value(CalibrationStep.RIGHT, movement)
+
+        cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.DOWN, movement))
+        cv2.waitKey()
+        self.process_calibration_value(CalibrationStep.DOWN, movement)
+
+        cv2.imshow("gotcha",self.make_calibration_screen(CalibrationStep.LEFT, movement))
+        cv2.waitKey()
+        self.process_calibration_value(CalibrationStep.LEFT, movement)
+
+        self.calibration_value.calc_delta()
+         
     def run(self):
         configuration = self.make_configuration()
 
